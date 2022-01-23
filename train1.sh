5f42a535e78f6e92ea8afc2bd73d35ca7474adf8
jiazewang <jzwang@link.cuhkedu.hk>
2022-01-24 02:55:27 +0800

    sh

---
+++ train1.sh
@@ -0,0 +1,11 @@
+OUTPUT_DIR=models.densetnt.1; \
+GPU_NUM=4; \
+python src/run.py --argoverse --future_frame_num 30 \
+  --do_train --data_dir train/data/ --output_dir output \
+  --hidden_size 128 --train_batch_size 64 --sub_graph_batch_size 4096 --use_map \
+  --core_num 16 --use_centerline --distributed_training 1 \
+  --other_params \
+    semantic_lane direction l1_loss \
+    goals_2D enhance_global_graph subdivide lazy_points new laneGCN point_sub_graph \
+    stage_one stage_one_dynamic=0.95 laneGCN-4 point_level point_level-4 \
+    point_level-4-3 complete_traj complete_traj-3 \
