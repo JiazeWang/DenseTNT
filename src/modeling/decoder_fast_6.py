03fa284428f5a0b4fd42bed07d4d302342f20b6d
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-02-17 14:40:41 +0800

    +mse

--- src/modeling/decoder_fast_6.py
+++ src/modeling/decoder_fast_6.py
@@ -138,7 +138,7 @@ class SetCriterion(nn.Module):
         class_loss = F.binary_cross_entropy(predict_class.float(), points_class.float())
         traj_loss = F.smooth_l1_loss(predict_traj.float(), gt_points.float())
         target_point = torch.from_numpy(target_point).to(device)
-        point_loss = F.smooth_l1_loss(predict_points.float(), target_point.float())
+        point_loss = F.mse_loss(predict_points.float(), target_point.float())
         total_loss = self.traj_loss_w*traj_loss+self.class_loss_w*class_loss + points_class
 
         index = torch.argmax(class_i).item()
