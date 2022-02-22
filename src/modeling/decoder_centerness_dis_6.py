b9a545a7abbac644af3341f0e607b424b3a43b7a
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-02-22 15:40:57 +0800

    Update decoder_centerness_dis_6.py

--- src/modeling/decoder_centerness_dis_6.py
+++ src/modeling/decoder_centerness_dis_6.py
@@ -175,7 +175,7 @@ class SetCriterion(nn.Module):
         #print("loss: ", total_points.shape, total_points_class.shape, coord_i.shape, class_i.shape, traj_i.shape)
         #centerness_gt = self.centerness_gt(total_points[0], coord_i).to(device)
         target_point = total_points[0]
-        #centerness_gt, distance_loss = self.distance_loss(target_point.to(device), coord_i.to(device))
+        centerness_gt, distance_loss = self.distance_loss(target_point.to(device), coord_i.to(device))
         #centerness_gt =centerness_gt.to(device)
         #distance_loss = distance_loss.to(device)
         #centerness_gt = centerness_gt.to(device)
