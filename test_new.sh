3cdea21fc728ca9d00e69596639fe013c97a3860
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-02-22 14:30:56 +0800

    Update test_new.sh

--- test_new.sh
+++ test_new.sh
@@ -1,6 +1,6 @@
-OUTPUT_DIR=output_new_matcher_one/test; \
+OUTPUT_DIR=centerness_v4_6/test; \
 GPU_NUM=1; \
-CUDA_VISIBLE_DEVICES=0 python src/run.py --argoverse --future_frame_num 30 \
+CUDA_VISIBLE_DEVICES=0 python src/run_centerness_6.py --argoverse --future_frame_num 30 \
   --do_train --data_dir train/data/ --output_dir ${OUTPUT_DIR} \
   --hidden_size 128 --train_batch_size 64 --sub_graph_batch_size 4096 --use_map \
   --core_num 16 --use_centerline --distributed_training ${GPU_NUM} \
@@ -9,6 +9,6 @@ CUDA_VISIBLE_DEVICES=0 python src/run.py --argoverse --future_frame_num 30 \
     goals_2D enhance_global_graph subdivide lazy_points new laneGCN point_sub_graph \
     stage_one stage_one_dynamic=0.95 laneGCN-4 point_level point_level-4 \
     point_level-4-3 complete_traj complete_traj-3 \
-  --model_recover_path output_new_matcher_one/model_save/model.59.bin \
+  --model_recover_path centerness_v4_6/model_save/model.60.bin \
   --reuse_temp_file --temp_file_dir output \
   --do_eval --eval_batch_size 16
