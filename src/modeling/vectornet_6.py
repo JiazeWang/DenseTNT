17a0a0c16ac929b8790612d5bb4c638444600c4e
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-02-24 11:26:58 +0800

    Update vectornet_6.py

--- src/modeling/vectornet_6.py
+++ src/modeling/vectornet_6.py
@@ -292,6 +292,7 @@ class VectorNet(nn.Module):
             agent_batch_input = agent_batch.unsqueeze(2)
             print("agent_batch_input.shape", agent_batch_input.shape)
             hist_out = self.hist_tf(agent_batch_input, self.query_batches, None, None)
+            print("hist_out.shape", hist_out.shape)
             lane_mem = self.lane_enc(self.lane_emb(lane_batch), lane_mask)
             lane_mem = lane_mem.unsqueeze(1).repeat(1, social_num, 1, 1)
             lane_mask = lane_mask.unsqueeze(1).repeat(1, social_num, 1, 1)
