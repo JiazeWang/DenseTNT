814046176efb104021f03e1f0e634c4d7cc747d7
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-03-11 11:04:33 +0800

    Update dataset_argoverse.py

--- src/dataset_argoverse.py
+++ src/dataset_argoverse.py
@@ -283,13 +283,14 @@ def preprocess(args, id2info, mapping):
 
     t = len(vectors)
     mapping['map_start_polyline_idx'] = len(polyline_spans)
-    print("vectors.shape", vectors)
+    print("vectors.shape", vectors[0].shape)
     if args.use_map:
         vectors, polyline_spans = get_sub_map(args, mapping['cent_x'], mapping['cent_y'], mapping['city_name'],
                                               vectors=vectors,
                                               polyline_spans=polyline_spans, mapping=mapping)
 
     print('len(vectors)', len(vectors))
+    print('vectors[0].shape', vectors[0].shape)
 
     matrix = np.array(vectors)
     #print("matrix.shape: ", matrix[0])
