8e78d04b013eaee6a59e9246f7cfc84d4d2507d7
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-03-11 10:54:59 +0800

    Create train_dataset.sh

---
+++ train_dataset.sh
@@ -0,0 +1,9 @@
+python src/run_centerness_6.py --argoverse --future_frame_num 30 \
+  --do_train --data_dir train/data/ --output_dir output_dataset \
+  --hidden_size 128 --train_batch_size 128 --sub_graph_batch_size 4096 --use_map \
+  --core_num 16 --use_centerline --distributed_training 4 \
+  --other_params \
+    semantic_lane direction l1_loss \
+    goals_2D enhance_global_graph subdivide lazy_points new laneGCN point_sub_graph \
+    stage_one stage_one_dynamic=0.95 laneGCN-4 point_level point_level-4 \
+    point_level-4-3 complete_traj complete_traj-3 \
